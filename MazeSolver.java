edead5b524c9e37fea09166e42e54acc650527d3
Sajed Nahian <snahian@stuy.edu>
2018-04-12 22:07:37 -0400

    Updated to match v0

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,48 +1,26 @@
 //Solve a given maze
 public class MazeSolver{
 	
-	public MazeSolver() {
+	private static int[] directions = {Maze.NORTH, Maze.EAST, Maze.SOUTH, Maze.WEST};
 
-	}
 
-	public static boolean isMazeSolvable(Maze current) {
-		System.out.println(current);
-		if (current.explorerIsOnA() == Maze.TREASURE) {
+	public static boolean isMazeSolvable(Maze maze) {
+		System.out.println(maze);
+		if (maze.explorerIsOnA() == Maze.TREASURE) 
 			return true;
-		} 
-		if (current.explorerIsOnA() == Maze.WALL) {
+		
+		if (maze.explorerIsOnA() == Maze.WALL) 
 			return false;
-		}
 		
-		Maze snapshot = new Maze(current);
-		current.dropA(Maze.WALL);
-
-		current.go(Maze.SOUTH);
-		if (isMazeSolvable(current)) {
-			return true;
+		for (int i = 0; i < directions.length; i++) {
+			Maze mazeCopy = new Maze(maze);
+			mazeCopy.dropA(Maze.WALL);
+			mazeCopy.go(directions[i]);
+			if (isMazeSolvable(mazeCopy)) 
+				return true;
 		}
-		current = snapshot;
 
-		current.go(Maze.NORTH);
-		if (isMazeSolvable(current)) {
-			return true;
-		}
-		current = snapshot;
-
-		current.go(Maze.EAST);
-		if (isMazeSolvable(current)) {
-			return true;
-		}
-		current = snapshot;
-
-
-		current.go(Maze.WEST);
-		if (isMazeSolvable(current)) {
-			return true;
-		}
-		//current = snapshot;
-
-		return false;
-		}
-		
+		return false;	
 	}
+
+}
